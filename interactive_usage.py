94cfd130de1b2771ee562b8150bd729e33384c32
zhengzhenyu <zhengzhenyu@bytedance.com>
2016-09-06 16:13:52 +0800

    添加interactive_usage.py
    Change-Id: Ib74dfaa025ceaa09f34e03968e5b4dc1f152c9d2

---
+++ interactive_usage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import tensorflow as tf
+
+
+def main():
+    sess = tf.InteractiveSession()
+
+    x = tf.Variable([1.0, 2.0])
+    a = tf.constant([3.0, 3.0])
+
+    x.initializer.run()
+
+    sub = tf.sub(x, a)
+    print sub.eval()
+
+    sess.close()
+
+if __name__ == '__main__':
+    main()
